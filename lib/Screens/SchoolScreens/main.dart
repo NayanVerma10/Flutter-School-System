5f8ea23fb1fab2e432e6253e03c9c800264d3085
NayanVerma10 <NayanVerma10@users.noreply.github.com>
2020-06-20 20:16:42 +0530

    Fixed the Floating Action Button
    The floating action button is available now in all the screens . And when you submit the details of anyone the form automatically resets

--- lib/Screens/SchoolScreens/main.dart
+++ lib/Screens/SchoolScreens/main.dart
@@ -4,22 +4,28 @@ import 'classes.dart';
 import 'management.dart';
 import 'profile.dart';
 import 'staff.dart';
+import 'addemployee.dart';
+import 'addteacher.dart';
+import 'addstd.dart';
+
 import '../Icons/iconss_icons.dart';
 import '../Icons/iconsss_icons.dart';
+
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 void main(String schoolCode) {
   runApp(MyApp(schoolCode));
 }
 
 class MyApp extends StatefulWidget {
-
-  String schoolCode;
+  final String schoolCode;
 
   MyApp(this.schoolCode);
 
-   @override
+  @override
   _MyAppState createState() => _MyAppState(schoolCode);
 }
+
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   String schoolCode;
@@ -32,70 +38,156 @@ class _MyAppState extends State<MyApp> {
       // theme: ThemeData.light(),
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
-        length: 5,      // Number of Tabs you want
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            bottom: TabBar(
-              isScrollable: true,
-              tabs: [   // Headings of each tab
-                Tab(icon: Icon(Iconsss.book_reader, size: 20),text: 'CLASSES'),
-                 Column(
-                  children: <Widget>[
-                  Container(
-                    height: 20,
-                    width: 30,
-                  child: Icon(Iconsss.user_cog, size: 20),
-                  ),
-                  SizedBox(height: 10,),
-                  Text('MANAGEMENT'),
-                  
-                  ]
-                  ),
-                Column(
-                  children: <Widget>[
-                  Icon(Iconsss.book, size: 20),
-                  SizedBox(height: 10,),
-                  Text('ACADEMICS')
-                  ]
-                  ),
-                  Column(
-                  children: <Widget>[
-                  Icon(Iconss.user_tie, size: 22),
-                  SizedBox(height: 9),
-                  Text('STAFF')
-                  ]
-                  ),
-                // Tab(icon: Icon(Iconss.user_tie),text: 'STAFF',),
-                // Tab(icon: Icon(Icons.person, size: 30,),text: 'PROFILE',)
-                Column(
-                  children: <Widget>[
+        length: 5, // Number of Tabs you want
+        child: Builder(
+          builder: (context) => Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              bottom: TabBar(
+                isScrollable: true,
+                tabs: [
+                  // Headings of each tab
+                  Tab(
+                      icon: Icon(Iconsss.book_reader, size: 20),
+                      text: 'CLASSES'),
+                  Column(children: <Widget>[
+                    Container(
+                      height: 20,
+                      width: 30,
+                      child: Icon(Iconsss.user_cog, size: 20),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('MANAGEMENT'),
+                  ]),
+                  Column(children: <Widget>[
+                    Icon(Iconsss.book, size: 20),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('ACADEMICS')
+                  ]),
+                  Column(children: <Widget>[
+                    Icon(Iconss.user_tie, size: 22),
+                    SizedBox(height: 9),
+                    Text('STAFF')
+                  ]),
+                  // Tab(icon: Icon(Iconss.user_tie),text: 'STAFF',),
+                  // Tab(icon: Icon(Icons.person, size: 30,),text: 'PROFILE',)
+                  Column(children: <Widget>[
                     Container(
                       height: 33,
-                  child: Icon(Icons.person, size: 33),
+                      child: Icon(Icons.person, size: 33),
                     ),
-                   SizedBox(height: 6),
-                  Text('PROFILE'),
-                  SizedBox(height: 8),
-                  ]
-                  ),
+                    SizedBox(height: 6),
+                    Text('PROFILE'),
+                    SizedBox(height: 8),
+                  ]),
+                ],
+              ),
+              title: Text(
+                'SCHOOL NAME',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                //What each tab will contain
+                Classes(schoolCode),
+                Management(),
+                Academics(),
+                Staff(),
+                Profile()
               ],
             ),
-            title: Text('SCHOOL NAME', style: TextStyle(fontSize: 20),),
-          ),
-          body: TabBarView(
-            children: [   //What each tab will contain
-             Classes(schoolCode),
-             Management(),
-             Academics(),
-             Staff(),
-             Profile()
-              
-            ],
+            floatingActionButton: SpeedDial(
+              // both default to 16
+              marginRight: 18,
+              marginBottom: 20,
+              animatedIcon: AnimatedIcons.menu_close,
+              animatedIconTheme: IconThemeData(size: 22.0),
+              // this is ignored if animatedIcon is non null
+              // child: Icon(Icons.add),
+
+              // If true user is forced to close dial manually
+              // by tapping main button and overlay is not rendered.
+              closeManually: false,
+              curve: Curves.bounceIn,
+              overlayColor: Colors.black,
+              overlayOpacity: 0,
+              onOpen: () => print('OPENING DIAL'),
+              onClose: () => print('DIAL CLOSED'),
+              tooltip: 'Menu',
+              heroTag: 'menu-hero-tag',
+              backgroundColor: Colors.black,
+              foregroundColor: Colors.white,
+              elevation: 8.0,
+              shape: CircleBorder(),
+              children: [
+                SpeedDialChild(
+                    child: Icon(Icons.person, size: 30, color: Colors.black),
+                    backgroundColor: Colors.grey[400],
+                    label: 'Add Employee',
+                    labelStyle: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AddEmployee(
+                                  schoolCode: schoolCode,
+                                )),
+                      );
+                    }),
+                SpeedDialChild(
+                    child: Icon(
+                      Iconss.user_tie,
+                      color: Colors.black,
+                    ),
+                    backgroundColor: Colors.grey[400],
+                    label: 'Add Teacher',
+                    labelStyle: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AddTeacher(
+                                  schoolCode: schoolCode,
+                                )),
+                      );
+                    }),
+                SpeedDialChild(
+                  child: Icon(
+                    Iconss.user_graduate,
+                    color: Colors.black,
+                  ),
+                  backgroundColor: Colors.grey[400],
+                  label: 'Add Student',
+                  labelStyle: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AddStd(
+                                schoolCode: schoolCode,
+                              )),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
