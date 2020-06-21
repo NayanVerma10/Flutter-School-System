409694de329c404914b708961311c4aac78a8926
NayanVerma10 <NayanVerma10@users.noreply.github.com>
2020-06-21 11:02:00 +0530

    Add Student updated (classes , sections and subjects)

--- lib/Screens/SchoolScreens/addstd.dart
+++ lib/Screens/SchoolScreens/addstd.dart
@@ -10,7 +10,6 @@ import 'package:image_picker/image_picker.dart';
 
 class AddStd extends StatefulWidget {
   final String schoolCode;
-
   AddStd({Key key, this.schoolCode}) : super(key: key);
 
   @override
@@ -24,10 +23,8 @@ class Item {
 
 class _AddStdState extends State<AddStd> {
 //  _formKey and _autoValidate
-  String schoolCode;
-
+  final String schoolCode;
   _AddStdState(this.schoolCode);
-
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool _autoValidate = false;
   String _firstname;
@@ -37,6 +34,7 @@ class _AddStdState extends State<AddStd> {
   String _dob;
   String _address;
   String _gender;
+  String section;
   String _rollno;
   String _fathername;
   String _mothername;
@@ -137,13 +135,15 @@ class _AddStdState extends State<AddStd> {
     const Item('XI'),
     const Item('XII'),
   ];
+  List<String> subjects = [];
 
-  List<Item> section = <Item>[
-    const Item('A'),
-    const Item('B'),
-    const Item('C'),
-    const Item('D'),
-  ];
+  int value = 1;
+
+  _addItem() {
+    setState(() {
+      value = value + 1;
+    });
+  }
 
   Widget editImage() {
     return Container(
@@ -187,10 +187,10 @@ class _AddStdState extends State<AddStd> {
             child: Container(
                 child: Column(
           children: <Widget>[
-            SizedBox(height: 20),
+            //SizedBox(height: 20),
 
             Padding(
-              padding: EdgeInsets.only(top: 10.0),
+              padding: EdgeInsets.only(top: 20.0),
               child: new Stack(fit: StackFit.loose, children: <Widget>[
                 new Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -212,7 +212,7 @@ class _AddStdState extends State<AddStd> {
                   ],
                 ),
                 Padding(
-                    padding: EdgeInsets.only(top: 60.0, right: 100.0),
+                    padding: EdgeInsets.only(top: 90.0, right: 100.0),
                     child: new Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
@@ -297,89 +297,13 @@ class _AddStdState extends State<AddStd> {
             //    )
             //     ]
             //      ) ,
-            SizedBox(height: 10),
+            //SizedBox(height: 10),
             Container(
                 margin: new EdgeInsets.all(15.0),
                 child: new Form(
                     key: _formKey,
                     autovalidate: _autoValidate,
                     child: Column(children: <Widget>[
-                      Row(children: <Widget>[
-                        DropdownButton<Item>(
-                          focusColor: Colors.grey[300],
-                          hint: Text(
-                            "Select class",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          value: selectedclass,
-                          onChanged: (Item value) {
-                            setState(() {
-                              selectedclass = value;
-                            });
-                          },
-                          items: classs.map((Item classs) {
-                            return DropdownMenuItem<Item>(
-                              value: classs,
-                              child: Row(
-                                children: <Widget>[
-                                  SizedBox(width: 10),
-                                  Text(
-                                    classs.name,
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                        SizedBox(width: 15),
-
-                        DropdownButton<Item>(
-                          focusColor: Colors.grey[300],
-                          hint: Text(
-                            "Select section",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          value: selectedsection,
-                          onChanged: (Item value) {
-                            setState(() {
-                              selectedsection = value;
-                            });
-                          },
-                          items: section.map((Item section) {
-                            return DropdownMenuItem<Item>(
-                              value: section,
-                              child: Row(
-                                children: <Widget>[
-                                  SizedBox(
-                                      width:
-                                          10), // change to 10 when uncomenting add section
-                                  Text(
-                                    section.name,
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                        //  SizedBox(width: 20,),
-                        FlatButton.icon(
-                            onPressed: () {
-                              section.add(Item('E'));
-                              print('added');
-                            },
-                            icon: Icon(Icons.add, color: Colors.black),
-                            label: Text(
-                              'Add section',
-                              style: TextStyle(color: Colors.black),
-                            ))
-                      ]),
-                      
                       new TextFormField(
                         decoration: const InputDecoration(
                             labelText: 'First Name',
@@ -478,7 +402,7 @@ class _AddStdState extends State<AddStd> {
                         cursorColor: Colors.black,
                         style:
                             TextStyle(height: 1.5, fontWeight: FontWeight.bold),
-                        validator: validateRollNo,
+                        validator: validateValue,
                         onSaved: (String val) {
                           _address = val;
                         },
@@ -514,12 +438,149 @@ class _AddStdState extends State<AddStd> {
                         cursorColor: Colors.black,
                         style:
                             TextStyle(height: 1.5, fontWeight: FontWeight.bold),
-                        validator: validateRollNo,
+                        validator: validateValue,
                         onSaved: (String val) {
                           _rollno = val;
                         },
                       ),
-                      SizedBox(height: 15),
+                      // SizedBox(height: 10),
+                      Row(children: <Widget>[
+                        Column(children: <Widget>[
+                          SizedBox(height: 20),
+                          Container(
+                            height: 45,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            decoration: BoxDecoration(
+                                color: Colors.grey[350],
+                                borderRadius: BorderRadius.circular(10)),
+
+                            child: DropdownButton<Item>(
+                              focusColor: Colors.grey[300],
+                              hint: Text(
+                                "Select class",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              value: selectedclass,
+                              onChanged: (Item value) {
+                                setState(() {
+                                  selectedclass = value;
+                                });
+                              },
+                              items: classs.map((Item classs) {
+                                return DropdownMenuItem<Item>(
+                                  value: classs,
+                                  child: Row(
+                                    children: <Widget>[
+                                      SizedBox(width: 10),
+                                      Text(
+                                        classs.name,
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                            //SizedBox(width: 15),
+
+                            //  SizedBox(width: 20,),
+                            // FlatButton.icon(onPressed: (){
+                            //   section.add(Item('E'));
+                            //   print('added');
+                            // },
+                            //  icon: Icon(Icons.add,color: Colors.black),
+                            //  label: Text('Add section',
+                            //  style: TextStyle(
+                            //    color: Colors.black
+                            //  ),
+                            //  ))
+                          )
+                        ]),
+                        SizedBox(width: 30),
+                        Flexible(
+                          child: new TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Section',
+                                hoverColor: Colors.black,
+                                focusColor: Colors.black),
+                            keyboardType: TextInputType.text,
+                            cursorColor: Colors.black,
+                            style: TextStyle(
+                                height: 1.5, fontWeight: FontWeight.bold),
+                            validator: validateValue,
+                            onSaved: (String val) {
+                              section = val;
+                            },
+                          ),
+                        )
+                      ]),
+
+                      //  new TextFormField(
+                      //    decoration: const InputDecoration(labelText: 'Subject',
+                      //    hoverColor: Colors.black,
+                      //    focusColor: Colors.black),
+                      //       keyboardType: TextInputType.text,
+                      //       cursorColor: Colors.black,
+                      //       style: TextStyle(height: 1.5,
+                      //       fontWeight: FontWeight.bold),
+                      //      // validator: validateMobile,
+                      //       onSaved: (String val) {
+                      //       _mobile = val;
+                      //         },
+                      // ),
+                      //  Column(
+                      //      children: <Widget>[
+                      //             SizedBox(
+
+                      //               height: 50*(_index.toDouble()),
+                      //                child: ListView(children: _myPets,
+                      //                //shrinkWrap: true
+                      //                ),
+                      //                )
+                      //               ],
+                      //             ),
+                      //     ListView(
+                      //  children: _listOfWidgets,
+                      //        ),
+                      // Expanded(
+                      //          child: SizedBox(
+                      //              height: 200.0,
+                      //             child: new ListView.builder(
+                      //            scrollDirection: Axis.horizontal,
+                      //             itemCount: _formdata.length,
+                      //             itemBuilder: (BuildContext ctxt, int index) {
+                      //             return _myPets[_index];
+                      //                      },
+                      //          ),
+                      //              ),
+                      //             ),
+                      ListView.builder(
+                          itemCount: this.value,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) =>
+                              this._buildRow(index)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            FlatButton(
+                              child: Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                              color: Colors.black87,
+                              padding: EdgeInsets.all(0),
+                              shape: CircleBorder(),
+                              disabledColor: Colors.grey,
+                              onPressed: _addItem,
+                            ),
+                          ]),
+
                       new TextFormField(
                         decoration: const InputDecoration(
                             labelText: 'Mobile',
@@ -658,38 +719,7 @@ class _AddStdState extends State<AddStd> {
                       //  )
                       FlatButton(
                           color: Colors.black,
-                          onPressed: () {
-                            if (_formKey.currentState.validate()) {
-                              //    If all data are correct then save data to out variables
-                              _formKey.currentState.save();
-                              createRecord();
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  // return object of type Dialog
-                                  return AlertDialog(
-                                    title: new Text("Added"),
-                                    content: new Text("The details for $_firstname have been added"),
-                                    actions: <Widget>[
-                                      // usually buttons at the bottom of the dialog
-                                      new FlatButton(
-                                        child: new Text("Close"),
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                          _formKey.currentState.reset();
-                                        },
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            } else {
-                              //    If all data are not valid then start auto validation.
-                              setState(() {
-                                _autoValidate = true;
-                              });
-                            }
-                          },
+                          onPressed: _validateInputs,
                           child: const Text('ADD NEW STUDENT',
                               style: TextStyle(
                                   fontSize: 16.0,
@@ -707,9 +737,9 @@ class _AddStdState extends State<AddStd> {
       return null;
   }
 
-  String validateRollNo(String value) {
+  String validateValue(String value) {
     if (value.length == 0)
-      return 'Enter valid roll number';
+      return 'Enter valid value';
     else
       return null;
   }
@@ -732,6 +762,36 @@ class _AddStdState extends State<AddStd> {
       return null;
   }
 
+  void _validateInputs() {
+    if (_formKey.currentState.validate()) {
+//    If all data are correct then save data to out variables
+      _formKey.currentState.save();
+      createRecord();
+
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+                title: new Text("Added"),
+                content:
+                    new Text("The details for $_firstname have been added"),
+                actions: <Widget>[
+                  new FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        _formKey.currentState.reset();
+                      },
+                      child: new Text("Close"))
+                ]);
+          });
+    } else {
+//    If all data are not valid then start auto validation.
+      setState(() {
+        _autoValidate = true;
+      });
+    }
+  }
+
   void createRecord() async {
     print(schoolCode);
     await databaseReference
@@ -745,26 +805,32 @@ class _AddStdState extends State<AddStd> {
       'rollno': _rollno,
       'gender': _gender,
       'class': selectedclass.name,
-      'section': selectedsection.name,
+      'section': section,
       'email': _email,
       'mobile': _mobile,
       'father \'s name': _fathername,
       'mother \'s name': _mothername,
       'dob': _dob,
-      'address': _address
-    }).whenComplete(() => print('Student Added'));
-
-    // DocumentReference ref = await databaseReference.collection("books")
-    //     .add({
-    //       'title': 'Flutter in Action',
-    //       'description': 'Complete Programming Guide to learn Flutter'
-    //     });
-    //print(ref.documentID);
+      'address': _address,
+      'subjects': subjects
+    }).whenComplete(() => print('Student added'));
   }
-//  @override
-//   userImage(File _image) {
-//     setState(() {
-//       this._image = _image;
-//     });
-//   }
+
+  _buildRow(int index) {
+    String ltext = "Subject" + (index + 1).toString();
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: ltext,
+          hoverColor: Colors.black,
+          fillColor: Colors.black,
+          focusColor: Colors.black),
+      keyboardType: TextInputType.text,
+      validator: validateValue,
+      cursorColor: Colors.black,
+      style: TextStyle(height: 1.5, fontWeight: FontWeight.bold),
+      onSaved: (String val) {
+        subjects.add(val);
+      },
+    );
+  }
 }
