8014477640e7b6b995d5175b730d09c7df2c00df
Ishitasinha7089 <34679447+Ishitasinha7089@users.noreply.github.com>
2020-06-23 18:45:20 +0530

    behavior added

--- lib/Screens/TeacherScreens/behavior.dart
+++ lib/Screens/TeacherScreens/behavior.dart
@@ -1,18 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:teacher_dashboard/classDetails.dart';
+import 'package:teacher_dashboard/iconssss_icons.dart';
+import 'package:teacher_dashboard/stdProfile.dart';
+import 'package:teacher_dashboard/iconss_icons.dart';
+import 'package:teacher_dashboard/setBehavior.dart';
+import 'dart:math' as math;
 
-class Behavior extends StatelessWidget {
+class Behavior extends StatefulWidget {
+  @override
+  _BehaviorState createState() => _BehaviorState();
+}
+ 
+//String className='X-A';
+
+class _BehaviorState extends State<Behavior> {
+
+  final stdName = [
+      'Lee Joon Gi', 'Cha Eun Woo', 'Lee Sung Kyung', 'Yang Ki Jong', 'Jang Ki Yong',
+      'Kim Tae Hyung', 'Kim Seo Woo', 'Jung Hae In', 'Bae Suzy', 'Chae Soo Bin', 'Jung Jin Yeong', 'Jung Ji Hyun'
+  ];
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Behavior',
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.w600
-          )
-        ),
-        ),
+    return Scaffold(
+     body: ListView.builder(
+        //itemCount: 10,
+        itemCount: stdName.length,
+        itemBuilder: (context, index) {
+         
+          return Card( //                           <-- Card widget
+            child: ListTile(              
+              title: Text(stdName[index],
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+              ),
+              leading: Icon(Iconss.user_graduate,
+              color: Colors.black,
+              size: 20,
+              ),
+              trailing: Icon(Icons.keyboard_arrow_right,
+              color: Colors.black,
+              ),
+              onTap: () { //                                  <-- onTap
+                  setState(() {
+                    
+                    // Navigator.push(context,
+                    //   MaterialPageRoute(builder: (context) => SetBehavior()));
+                    showDialog(
+                context: context,
+                builder: (context) {                                     
+                  return Dialog(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+                    elevation: 16,  
+                     child: SingleChildScrollView(                 
+                    child: Container(
+                      height: 500.0,
+                      width: 700.0,          
+                        child: 
+                              SetBehavior()
+                             // ])
+                    )
+                    )
+                  );
+                }
+                    );
+                      });
+                  }
+              
+            //  leading: Icon(icons[index]),
+             // title: Text(titles[index]),
+            ),
+          );
+        },
+      )
     );
   }
 }
